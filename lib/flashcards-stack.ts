95c2ac9c4146034b63fa6b7a5b7a697152dc2d85
Ana Cunha <1771610+anacunha@users.noreply.github.com>
2022-11-22 12:04:20 -0300

    Create Amplify exported backend

--- lib/flashcards-stack.ts
+++ lib/flashcards-stack.ts
@@ -1,10 +1,16 @@
 import * as cdk from 'aws-cdk-lib';
+import * as path from 'path';
 import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
+import { AmplifyExportedBackend } from '@aws-amplify/cdk-exported-backend';
 
 export class FlashcardsStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
+
+    const amplifyBackend = new AmplifyExportedBackend(this, 'AmplifyBackend', {
+      amplifyEnvironment: 'dev',
+      path: path.resolve(__dirname, 'amplify-export-flashcards'),
+    });
 
     // The code that defines your stack goes here
 
