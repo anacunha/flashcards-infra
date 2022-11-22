bf83e66ec613cc0aa026fd763025c009d5707619
Ana Cunha <1771610+anacunha@users.noreply.github.com>
2022-11-22 12:53:44 -0300

    Add trigger to build Amplify app on create

--- lib/flashcards-stack.ts
+++ lib/flashcards-stack.ts
@@ -1,19 +1,38 @@
 import * as cdk from 'aws-cdk-lib';
 import { Construct } from 'constructs';
 import * as amplify from '@aws-cdk/aws-amplify-alpha';
+import * as customResource from 'aws-cdk-lib/custom-resources';
 
 export class FlashcardsStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
+
+    const branch = 'main';
 
     const amplifyApp = new amplify.App(this, 'FlashcardsApp', {
       sourceCodeProvider: new amplify.GitHubSourceCodeProvider({
         owner: 'anacunha',
         repository: 'flashcards-app',
         oauthToken: cdk.SecretValue.secretsManager('github-token'),
-      })
+      }),
     });
 
-    amplifyApp.addBranch('main');
+    amplifyApp.addBranch(branch);
+
+    const buildTrigger = new customResource.AwsCustomResource(this, 'BuildAmplifyAppTrigger', {
+      onCreate: {
+        service: 'Amplify',
+        action: 'startJob', // https://docs.aws.amazon.com/AWSJavaScriptSDK/latest/AWS/Amplify.html#startJob-property
+        parameters: {
+          appId: amplifyApp.appId,
+          branchName: branch,
+          jobType: 'RELEASE',
+        },
+        physicalResourceId: customResource.PhysicalResourceId.of('BuildAmplifyAppTrigger'),
+      },
+      policy: customResource.AwsCustomResourcePolicy.fromSdkCalls({
+        resources: customResource.AwsCustomResourcePolicy.ANY_RESOURCE,
+      }),
+    });
   }
 }
