fa25436fbc2117a6549efe59e763083f0126bdaa
Ana Cunha <1771610+anacunha@users.noreply.github.com>
2022-11-26 12:40:54 -0500

    Revert "Create exported backend S3 bucket"
    This reverts commit 709f978feebef44586c5081101cc59954182f431.

--- lib/flashcards-stack.ts
+++ lib/flashcards-stack.ts
@@ -1,34 +1,16 @@
 import * as cdk from 'aws-cdk-lib';
-// import * as path from 'path';
+import * as path from 'path';
 import { Construct } from 'constructs';
-// import { AmplifyExportedBackend } from '@aws-amplify/cdk-exported-backend';
-import * as iam from 'aws-cdk-lib/aws-iam';
-import * as s3 from 'aws-cdk-lib/aws-s3';
+import { AmplifyExportedBackend } from '@aws-amplify/cdk-exported-backend';
 
 export class FlashcardsStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    const appAcountUserArn = new cdk.CfnParameter(this, 'AppAccountUserArn', {
-      type: 'String',
-      description: 'The ARN of the user in the App account that will upload the exported backend files to S3',
+    const amplifyBackend = new AmplifyExportedBackend(this, 'AmplifyBackend', {
+      // amplifyEnvironment: cdk.Stack.of(this).region + cdk.Stack.of(this).account,
+      amplifyEnvironment: 'prod',
+      path: path.resolve(__dirname, 'amplify-export-flashcardsapp'),
     });
-
-    const exportedBackendBucket = new s3.Bucket(this, 'ExportedBackendBucket', {
-      bucketName: 'amplify-backend-flashcards',
-      removalPolicy: cdk.RemovalPolicy.DESTROY,
-    });
-
-    exportedBackendBucket.addToResourcePolicy(new iam.PolicyStatement({
-      principals: [new iam.ArnPrincipal(appAcountUserArn.valueAsString)],
-      actions: ['s3:PutObject'],
-      resources: [exportedBackendBucket.arnForObjects('export-amplify-stack.zip')],
-    }));
-
-    // const amplifyBackend = new AmplifyExportedBackend(this, 'AmplifyBackend', {
-    //   // amplifyEnvironment: cdk.Stack.of(this).region + cdk.Stack.of(this).account,
-    //   amplifyEnvironment: 'prod',
-    //   path: path.resolve(__dirname, 'amplify-export-flashcards'),
-    // });
   }
 }
