c0c487aa589c3632b44268a45216a7b2d24426e3
Ana Cunha <1771610+anacunha@users.noreply.github.com>
2022-11-30 20:43:34 -0800

    Add environment variables to Amplify app

--- lib/flashcards-stack.ts
+++ lib/flashcards-stack.ts
@@ -4,18 +4,20 @@ import { Construct } from 'constructs';
 import { AmplifyExportedBackend } from '@aws-amplify/cdk-exported-backend';
 import * as amplify from '@aws-cdk/aws-amplify-alpha';
 import * as customResource from 'aws-cdk-lib/custom-resources';
+import { CfnIdentityPool } from 'aws-cdk-lib/aws-cognito';
 
 export class FlashcardsStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
     const amplifyBackend = new AmplifyExportedBackend(this, 'AmplifyBackend', {
-      // amplifyEnvironment: cdk.Stack.of(this).region + cdk.Stack.of(this).account,
       amplifyEnvironment: 'prod',
       path: path.resolve(__dirname, 'amplify-export-flashcards'),
     });
 
     const branch = 'main';
+    const identityPool = amplifyBackend.authNestedStack().identityPool();
+    const identityProviders = identityPool.cognitoIdentityProviders as CfnIdentityPool.CognitoIdentityProviderProperty[];
 
     const amplifyApp = new amplify.App(this, 'FlashcardsApp', {
       sourceCodeProvider: new amplify.GitHubSourceCodeProvider({
@@ -23,6 +25,13 @@ export class FlashcardsStack extends cdk.Stack {
         repository: 'flashcards-app',
         oauthToken: cdk.SecretValue.secretsManager('github-token'),
       }),
+      environmentVariables: {
+        'REGION': amplifyBackend.rootStack.region,
+        'IDENTITY_POOL_ID': identityPool.ref,
+        'USER_POOL_ID': amplifyBackend.authNestedStack().userPool().ref,
+        'USER_POOL_CLIENT_ID': identityProviders[0]?.clientId as string,
+        'GRAPHQL_ENDPOINT': amplifyBackend.graphqlNestedStacks().graphQLAPI().attrGraphQlUrl,
+      },
     });
 
     amplifyApp.addBranch(branch);
